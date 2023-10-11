74192ab6124f5459d427cbda0a39963304d589b7
trinhvanduong <duongtv@templaza.com>
2023-10-11 16:18:12 +0700

    Update version 2.6.8

--- setup/packages/addons/extrafields/dropdownlist/tmpl/input.php
+++ setup/packages/addons/extrafields/dropdownlist/tmpl/input.php
@@ -20,7 +20,33 @@
 //no direct access
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+
 $this -> setAttribute('class', 'custom-select', 'input');
-//if ($options) {
-    echo JHtml::_('select.genericlist', $options, $this->getName(), $this->getAttribute(null, null, "input"), 'value', 'text', $value, $this->getId());
-//}
+
+$html = JHtml::_('select.genericlist', $options, $this->getName(), $this->getAttribute(null, null, "input"), 'value', 'text', $value, $this->getId());
+
+if(version_compare(JVERSION, '4.0', '<')){
+    echo $html;
+}else{
+
+    $attr2  = '';
+    $attr2 .= ' placeholder="' . Text::_('JGLOBAL_TYPE_OR_SELECT_SOME_OPTIONS') . '" ';
+
+    if ($this -> isRequired()) {
+        $attr  .= ' required class="required"';
+        $attr2 .= ' required';
+    }
+
+    Text::script('JGLOBAL_SELECT_NO_RESULTS_MATCH');
+    Text::script('JGLOBAL_SELECT_PRESS_TO_SELECT');
+
+    Factory::getApplication()->getDocument()->getWebAssetManager()
+        ->usePreset('choicesjs')
+        ->useScript('webcomponent.field-fancy-select');
+
+    ?>
+    <joomla-field-fancy-select <?php echo $attr2; ?>><?php echo $html; ?></joomla-field-fancy-select>
+    <?php
+}
