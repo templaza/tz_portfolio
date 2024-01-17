3cd9802e5ddb3c5085b9cdeb1ac6cb32a87a705a
trinhvanduong <duongtv@templaza.com>
2024-01-17 15:35:11 +0700

    Fix issue save acl with Joomla 4.x and Joomla 5.x

--- setup/packages/admin/views/acl/tmpl/edit.php
+++ setup/packages/admin/views/acl/tmpl/edit.php
@@ -23,33 +23,34 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\Uri\Uri;
 
 $user		= Factory::getUser();
 $document   = Factory::getApplication() -> getDocument();
 ?>
 <form name="adminForm" id="adminForm" method="post" action="<?php
-    echo JRoute::_('index.php?option=com_tz_portfolio_plus&view=acl&layout=edit&section='
+echo JRoute::_('index.php?option=com_tz_portfolio_plus&view=acl&layout=edit&section='
     .$this->state->get('acl.section')); ?>">
     <div class="tpContainer ">
         <?php if(!COM_TZ_PORTFOLIO_PLUS_JVERSION_4_COMPARE){ ?>
         <div class="control-group">
-        <?php }?>
+            <?php }?>
             <?php echo $this->form->getInput('rules'); ?>
-        <?php if(!COM_TZ_PORTFOLIO_PLUS_JVERSION_4_COMPARE){ ?>
+            <?php if(!COM_TZ_PORTFOLIO_PLUS_JVERSION_4_COMPARE){ ?>
         </div>
-        <?php }?>
+    <?php }?>
 
         <?php
         if(version_compare(JVERSION, '4.0', '>=')) {
-            $scriptOptions  = $document -> getScriptOptions('webcomponents');
-            $scriptOptions = array_map (function($value){
-                if(strpos($value, JUri::root(true).'/media/system/js/fields/joomla-field-permissions.js') !== false){
-                    return JUri::base(true).'/components/com_tz_portfolio_plus/js/tpp-field-permissions.min.js';
-                }
-                return $value;
-            },$scriptOptions );
+            $wa = $document->getWebAssetManager();
 
-            $document->addScriptOptions('webcomponents', $scriptOptions, false);
+            /* Remove joomla-field-permissions javascript file link */
+            $wa->disableAsset("script", "webcomponent.field-permissions");
+
+            /* Add my field-permissions javascript file link */
+            $wa -> registerAndUseScript('com_tz_portfolio_plus.field-permissions',
+                Uri::base().'/components/com_tz_portfolio_plus/js/tpp-field-permissions.min.js',
+                ['version' => 'auto', 'relative' => false], ['defer' => true]);
         }else{
             $document -> addScriptVersion(JUri::base(true).'/components/com_tz_portfolio_plus/js/tpp-field-permissions.min.js', array('version' => 'auto'));
         }
